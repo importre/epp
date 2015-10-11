0e744c34bd4b0a3e97f743d2ed86ea3803bc5a13
Jaewe Heo <jaeweheo@gmail.com>
2015-10-11 16:25:26 +0900

    abs -> rel path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'app/main.cjsx'),
   output: {
     path: path.resolve(__dirname, 'out'),
-    publicPath: '/out/',
+    publicPath: 'out/',
     filename: 'app.js'
   },
   module: {
